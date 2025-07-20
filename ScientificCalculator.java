00a9d088cf69d2f0f7ad4adf19125705ab82f953
rayan <rayanalhashmi8@gmail.com>
2025-07-20 11:39:21 +0400

    feat: Initial setup for Scientific Calculator

---
+++ ScientificCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ScientificCalculator {
+
+    public static void main(String[] args) {
+    }
+
+    // --- Menu Display Method ---
+    public static void displayMenu() {
+    }
+}
+
+
